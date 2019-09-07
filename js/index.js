16b14a5fd843bd360498f0826d96df08afc18c73
Baher-essam <44957599+Baher-essam@users.noreply.github.com>
2019-09-07 23:24:23 +0200

    Section 1 update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 
-
+//loading start function
 $(document).ready(function(){
     $("#loading").fadeOut(2000);
     $(".sk-cube-grid").fadeOut(2000 , function(){
@@ -8,3 +8,24 @@ $(document).ready(function(){
     })
 })
     
+
+
+//owl crasuol for parients section1
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
