d97c1527fcfb61de52bd8fbc50c285d066f09586
Baher-essam <44957599+Baher-essam@users.noreply.github.com>
2019-09-07 17:14:57 +0200

    JS upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+
+
+$(document).ready(function(){
+    $("#loading").fadeOut(2000);
+    $(".sk-cube-grid").fadeOut(2000 , function(){
+        $("html").css("overflow" , "auto");
+    })
+})
+    
